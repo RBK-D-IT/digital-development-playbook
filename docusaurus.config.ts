0f2b4e467713ee81e6688bd1aa12bb5d126cecb4
cartochris <01_humble_glossy@icloud.com>
2024-09-06 15:45:08 +0100

    chore: update edit page link to repo

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -40,7 +40,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/RBK-D-IT/digital-development-playbook',
         },
         blog: false, // Optional: disable the blog plugin
         theme: {
